10722f0ea961f692a3b5ef91a6e0d8753356c0bb
naveenvupputuri <vupputuri.naveen1@gmail.com>
2015-03-06 10:15:53 -0800

    Cool

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by akhilesh kalaru on 2/25/15.
+//  Copyright (c) 2015 Naveen Sai Vupputuri. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
