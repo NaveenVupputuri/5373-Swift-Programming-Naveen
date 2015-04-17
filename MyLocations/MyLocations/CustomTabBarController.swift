cd9f961bed11067006077206c7265cca0acb7f09
N. Vupputuri <nvupputuri@BO120-07-46048.local>
2015-04-17 13:19:01 -0500

    MyLocations

---
+++ MyLocations/MyLocations/CustomTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  CustomTabBarController.swift
+//  MyLocations
+//
+//  Created by N. Vupputuri on 4/17/15.
+//  Copyright (c) 2015 mwsu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// This class holds the data for my model.
+class ModelData {
+    var latit = 0.00
+    var longit = 0.00
+}
+
+class CustomTabBarController: UITabBarController {
+    
+    // Instantiate the one copy of the model data that will be accessed
+    // by all of the tabs.
+    var model = ModelData()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+}
