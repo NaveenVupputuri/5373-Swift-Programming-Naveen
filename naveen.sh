bbe32797089a86fa70f9356a812e7cd7c4ca8f0c
NaveenVupputuri <NaveenVupputuri@users.noreply.github.com>
2016-02-10 12:31:55 -0600

    Update naveen.sh

--- naveen.sh
+++ naveen.sh
@@ -1,3 +1,18 @@
-njdbbjbcl/a
-nsbcjlnl
-njslacljnl
+
+#!/bin/bash
+
+# Script to Print a Random word from file
+
+wordfile="/usr/share/dict/words"
+
+#data=$(cat "$path")
+#lines='wc -l $data | cut -d -f1'
+#random_word='date + %N'
+#number= $[( random_word % $lines ) + 1]
+#selectedword='sed -n $number\p $ data'
+
+random==$$;
+lines=$(cat $wordfile | wc -l);
+rnum=((random*random%$lines+1));
+sed -n "$rnum p" $wordfile;
+
